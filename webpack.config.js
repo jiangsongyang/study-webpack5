fb0e7bf56920aa12f14f4d3da7bdd1dc9d251120
jiangsongyang <2744856707@qq.com>
2022-08-20 16:07:53 +0800

    开发环境

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,15 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  mode: "development",
+  devtool: "inline-source-map",
+  devServer: {
+    port: 9090,
+    static: "./dist",
+  },
+  optimization: {
+    runtimeChunk: "single",
+  },
   entry: {
     index: "./src/index.js",
     print: "./src/print.js",
@@ -10,13 +19,14 @@ module.exports = {
     clean: true,
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
+    publicPath: '/',
   },
   module: {
     rules: [],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "管理输出",
+      title: "development",
     }),
   ],
 };
