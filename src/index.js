f8964411f7ce9ae7d2069d1221a88c7ce1d1fd4b
jiangsongyang <2744856707@qq.com>
2022-08-20 15:49:01 +0800

    资源相关

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-import _ from 'lodash'
+import _ from "lodash";
+import "./style.css";
+import MyImage from "./test.jpeg";
+import Data from "./data.xml";
+import Notes from "./data.csv";
 
 function component() {
- const element = document.createElement('div');
+  const element = document.createElement("div");
 
- // lodash（目前通过一个 script 引入）对于执行这一行是必需的
- element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  // lodash（目前通过一个 script 引入）对于执行这一行是必需的
+  element.innerHTML = _.join(["Hello", "webpack"], " ");
 
- return element;
+  const myIcon = new Image();
+  myIcon.src = MyImage;
+
+  console.log(Data);
+  console.log(Notes);
+
+  element.appendChild(myIcon);
+
+  return element;
 }
 
 document.body.appendChild(component());
